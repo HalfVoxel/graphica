ff38c38c0443ba8301012f7c74fc4bff0eaef4d7
Aron Granberg <aron.granberg@gmail.com>
2021-03-17 19:32:18 +0100

    Add missing file

---
+++ src/vertex.rs
@@ -0,0 +1,19 @@
+use lyon::math::Point;
+
+pub trait GPUVertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a>;
+}
+
+impl GPUVertex for Point {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as u64,
+            step_mode: wgpu::InputStepMode::Vertex,
+            attributes: &[wgpu::VertexAttribute {
+                offset: 0,
+                format: wgpu::VertexFormat::Float2,
+                shader_location: 0,
+            }],
+        }
+    }
+}
